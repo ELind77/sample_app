d63515681b2d69a6b9eba936d57713b6d0d67ed8
Eric Lind <Eric.Lind.7@gmail.com>
2014-06-04 00:33:38 -0400

    Finish static pages. Chapter 3git add .

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,45 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+    # home Page
+    describe"Home page" do
+        it "should have the content 'Sample App'" do
+            visit '/static_pages/home'
+            expect(page).to have_content('Sample App')
+        end
+        
+        # Title test
+        it "should have the title 'Home'" do
+            visit '/static_pages/home'
+            expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+        end
+    end
+    
+    # Help Page
+    describe "Help page" do
+        it "should have the content 'Help'" do
+            visit '/static_pages/help'
+            expect(page).to have_content('Help')
+        end
+        
+        # Title
+        it "should have the title 'Help'" do
+          visit '/static_pages/help'
+          expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+        end
+    end
+    
+    # About Page
+    describe "About page" do
+        it "should have the content 'About Us'" do
+            visit '/static_pages/about'
+            expect(page).to have_content('About Us')
+        end
+        # Title Test
+        it "should have the title 'About Us'" do
+          visit '/static_pages/about'
+          expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+        end
+    end
+end
+  
